8fbc4b82bcddb003b6b1b4b1c4906397ba3f56e8
stmeem <stmeem@yahoo.com>
2022-11-07 01:46:21 +0600

    Add loader into category

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from 'next/router';
 import { getCategories, getCategoryPost } from "../../queries";
-import { PostCard, Categories } from "../../components";
+import { Loader, PostCard, Categories } from "../../components";
 
 const CategoryPost = ({ posts }) => {
     const router = useRouter();
